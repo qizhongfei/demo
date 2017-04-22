59c4a3284f4dc0a7936f02fbc50b5e4791e8b966
qizhongfei <qizhongfei2010@hotmail.com>
2017-04-22 18:30:41 +0800

    接口默认就被abstract修饰只是省略了

---
+++ src/ios/TestIntface.java
@@ -0,0 +1,12 @@
+package ios;
+
+/** 
+* @ClassName: TestIntface 
+* @Description: 接口默认就被abstract修饰只是省略了
+* @author qizhongfei
+* @date 2017年4月22日 下午6:29:22 
+*  
+*/
+public abstract interface TestIntface {
+
+}
