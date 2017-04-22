26502be4360c4de19a06205d04dab5c6beb10d63
qizhongfei <qizhongfei2010@hotmail.com>
2017-04-22 18:31:04 +0800

    使用transient关键字不序列化某个变量  *        注意读取的时候，读取数据的顺序一定要和存放数据的顺序保持一致

---
+++ src/ios/TransientTest.java
@@ -0,0 +1,96 @@
+package ios;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+/**
+ * @description 使用transient关键字不序列化某个变量
+ *        注意读取的时候，读取数据的顺序一定要和存放数据的顺序保持一致
+ *        
+ * @author Alexia
+ * @date  2013-10-15
+ */
+public class TransientTest {
+	
+	public static void test(User user){
+		user.abc = "123";
+	}
+    
+    public static void main(String[] args) {
+        
+        User user = new User();
+        user.setUsername("Alexia");
+        user.setPasswd("123456");
+        user.abc = "899";
+        System.out.println(user.abc);
+        test(user);
+        System.out.println(user.abc);
+        
+        
+        System.out.println("read before Serializable: ");
+        System.out.println("username: " + user.getUsername());
+        System.err.println("password: " + user.getPasswd());
+        
+        try {
+            ObjectOutputStream os = new ObjectOutputStream(
+                    new FileOutputStream("C:/user.txt"));
+            os.writeObject(user); // 将User对象写进文件
+            os.flush();
+            os.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            // 在反序列化之前改变username的值
+            User.username = "jmwang";
+            
+            ObjectInputStream is = new ObjectInputStream(new FileInputStream(
+                    "C:/user.txt"));
+            user = (User) is.readObject(); // 从流中读取User的数据
+            is.close();
+            
+            System.out.println("\nread after Serializable: ");
+            System.out.println("username: " + user.getUsername());
+            System.err.println("password: " + user.getPasswd());
+            
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class User implements Serializable {
+    private static final long serialVersionUID = 8294180014912103005L;  
+    
+    
+    public String abc ;
+    public static String username;
+    private transient String passwd;
+    
+    public String getUsername() {
+        return username;
+    }
+    
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+    public String getPasswd() {
+        return passwd;
+    }
+    
+    public void setPasswd(String passwd) {
+        this.passwd = passwd;
+    }
+
+}
